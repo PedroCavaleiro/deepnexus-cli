c878969a8d30f3e8967f94e8df0220f7ee7e7d8c
Pedro Cavaleiro <p.cavaleiro@icloud.com>
2025-05-20 17:24:26 +0000

    Adds link to storcli docs

--- diskmanagement/sas.py
+++ diskmanagement/sas.py
@@ -1,5 +1,8 @@
 from ..utils import run_command
 
+# Commands supported by storcli64
+# https://techdocs.broadcom.com/us/en/storage-and-ethernet-connectivity/enterprise-storage-solutions/storcli-12gbs-megaraid-tri-mode/1-0/v11869215/v11673749/v11675603/v11675913.html
+
 def show_sas_all():
     output = run_command("/opt/MegaRAID/storcli/storcli64 /call/sall show")
     print(output)
