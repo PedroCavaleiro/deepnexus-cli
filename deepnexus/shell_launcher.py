d198c08d0f10f736468cb16d298573eb01cf10b7
Pedro Cavaleiro <p.cavaleiro@icloud.com>
2025-05-22 16:54:14 +0000

    Fixes shell_launcher exit

--- deepnexus/shell_launcher.py
+++ deepnexus/shell_launcher.py
@@ -6,12 +6,12 @@ from deepnexus.utils import status_message, Status
 def open_shell(app_config):
     home_dir = os.path.expanduser("~")
     os.chdir(home_dir)
-    
-    try:
-        subprocess.run(os.environ.get("SHELL", app_config["shell"]), check=True)
-    except subprocess.CalledProcessError as e:
-        print(f"{status_message(Status.ERROR)} Shell exited with error: {e}")
-    
+
+    shell = os.environ.get("SHELL", app_config["shell"])
+    os.system(shell)
+
+    os.system('clear')
+
     python = sys.executable
     root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
     script_path = os.path.join(root_dir, "deepnexus-cli.py")
