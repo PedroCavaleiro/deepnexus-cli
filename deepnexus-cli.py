0dd04e76012dc9e92655e4e0f9378c3bd2cc7be6
Pedro Cavaleiro <p.cavaleiro@icloud.com>
2025-05-23 09:21:46 +0000

    Forces current dir

--- deepnexus-cli.py
+++ deepnexus-cli.py
@@ -2,10 +2,15 @@ from deepnexus.menus import main_menu
 from deepnexus.utils import load_config
 from deepnexus.vars import APP_CONFIG_PATH
 import pyfiglet
+import os
+import sys
 from deepnexus.escape import Ansi
 font = Ansi.escape
 
 def main():
+
+    root_dir = os.path.abspath(os.path.dirname(__file__))
+    os.chdir(root_dir)
 
     config = load_config(APP_CONFIG_PATH)
     ascii_art = pyfiglet.figlet_format(config["banner"]) # type: ignore
