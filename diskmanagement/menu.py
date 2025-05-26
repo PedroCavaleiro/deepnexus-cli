3b6486f2cc6a518b054c9594827d0a6072851de4
Pedro Cavaleiro <p.cavaleiro@icloud.com>
2025-05-26 15:04:58 +0000

    removes unavailable imports

--- diskmanagement/menu.py
+++ diskmanagement/menu.py
@@ -2,7 +2,7 @@ from deepnexus.helpmenus import command_not_found
 from diskmanagement.helpmenu import disks_help, sas_submenu_help
 from deepnexus.vars import COLORS, DISKS_CONFIG_PATH
 from deepnexus.utils import load_config, get_prompt_text, clear_screen, status_message, Status
-from diskmanagement.disks import show_all_disks, show_mounted_disks, prepare_new_disk, locate_disk, mount_disk
+from diskmanagement.disks import show_all_disks, prepare_new_disk, locate_disk, mount_disk
 from diskmanagement.sas import show_sas_all, show_sas_disk, show_disk_smart
 
 def disks_menu(app_config):
